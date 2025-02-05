b3950b853162ec0b48cacb302a0c10a8afc70821
ibad-cr <smashingfour.1888@gmail.com>
2025-02-05 16:44:58 +0400

    test7

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: process.env.CLIENT_URL || "http://localhost:3000", // VITE frontend URL
+        origin: process.env.CLIENT_URL || "http://localhost:3000",
         methods: ["GET", "POST"],
         credentials: true,
       }
